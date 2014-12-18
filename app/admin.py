ecc4f7c3dbe4707bb5d4a7bbc55886b725b14e5d
Naffer Reyes <reyesnaffer@gmail.com>
2014-12-18 12:44:50 -0430

    funciones de administrador avanzadas
    Agregadas funciones avanzadas de administrador, filtros, imágenes de
    votos y nuevo modelo con relación muchos a muchos

--- app/admin.py
+++ app/admin.py
@@ -1,6 +1,36 @@
 from django.contrib import admin
-from app.models import *
+from app.models import Enlace, Categoria, Agregador
+
+from app.actions import export_as_csv
 # Register your models here.
 
-admin.site.register(Categoria)
-admin.site.register(Enlace)
+class EnlaceAdmin(admin.ModelAdmin):
+	list_display = ('titulo', 'enlace', 'categoria', 'imagen_voto', 'es_popular')
+	list_filter = ('categoria', 'usuario')
+	search_fields = ('categoria__titulo', 'usuario__email')
+	list_editable = ('titulo', 'enlace', 'categoria')
+	actions = [export_as_csv]
+	raw_id_fields = ('categoria', 'usuario')
+
+	def imagen_voto(self, obj):
+		u = obj.mis_votos_en_imagen_rosada()
+		tag = '<img src="%s" />' %(u)
+		return tag
+	imagen_voto.allow_tags = True
+	imagen_voto.admin_order_field = "votos"
+
+class EnlaceInline(admin.StackedInline):
+	model = Enlace
+	extra = 3
+	raw_id_fields = ('usuario',)
+
+class CategoriaAdmin(admin.ModelAdmin):
+	actions = [export_as_csv]
+	inlines = [EnlaceInline]
+
+class AgregadorAdmin(admin.ModelAdmin):
+	filter_horizontal = ('enlaces',)
+
+admin.site.register(Agregador, AgregadorAdmin)
+admin.site.register(Categoria, CategoriaAdmin)
+admin.site.register(Enlace, EnlaceAdmin)
--- app/models.py
+++ app/models.py
@@ -18,3 +18,14 @@ class Enlace(models.Model):
 
 	def __str__(self):
 		return "{0} - {1}" .format(self.titulo, self.enlace)
+
+	def mis_votos_en_imagen_rosada(self):
+		return 'http://placehold.it/200x100/E8117F/ffffff/&text=%d+votos' % self.votos
+
+	def es_popular(self):
+		return self.votos > 10
+	es_popular.boolean = True
+
+class Agregador(models.Model):
+	titulo = models.CharField(max_length=140)
+	enlaces = models.ManyToManyField(Enlace)
